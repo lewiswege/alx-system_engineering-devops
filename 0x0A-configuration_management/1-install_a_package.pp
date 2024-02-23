d57e0554fcda50483d937142023ce3040cd03e4c
lewiswege <wegelewis7@gmail.com>
2024-02-23 17:47:09 +0300

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,7 @@
-# 1-install_a_package.pp
-
-class { 'python':
-  version => 'system',
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'Flask':
+#!/usr/bin/pup
+# Using Puppet, install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
