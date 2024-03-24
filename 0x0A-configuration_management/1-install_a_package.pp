841f30f8d2723fc6b4c9519eeda78b0909d140ee
lewis wege <wegelewis7@gmail.com>
2024-03-24 17:59:28 +0300

    restored new code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-#!/usr/bin/pup
-# Using Puppet, install flask from pip3
-package { 'python3-pip':
-  ensure => installed,
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  environment => ['PATH=/usr/local/bin:/usr/bin:/bin'],
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-}
-
