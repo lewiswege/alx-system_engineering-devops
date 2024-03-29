cc0efd909af0cfaefaaa5515fbed5029de2709af
lewis wege <wegelewis7@gmail.com>
2024-03-29 11:22:27 +0300

    improved code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
+
 if __name__ == '__main__':
     employeeId = sys.argv[1]
-    mainUrl = "https://jsonplaceholder.typicode.com/users"
-    url = mainUrl + "/" + employeeId
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
     employeeName = response.json().get('name')
@@ -13,15 +16,16 @@ if __name__ == '__main__':
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
     tasks = response.json()
-    done = 0 
-    done_task = []
+    done = 0
+    done_tasks = []
 
     for task in tasks:
         if task.get('completed'):
-            done_task.append(task)
+            done_tasks.append(task)
             done += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(employeeName, done, len(tasks)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-    for task in done_task:
+    for task in done_tasks:
         print("\t {}".format(task.get('title')))
