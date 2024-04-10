96973cfb6d6a9d0d87568cb697232c583c9311da
lewis wege <wegelewis7@gmail.com>
2024-04-10 16:57:24 +0300

    code refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,27 @@
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
+
 from requests import get
 
+
 def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
+    results = response.json()
 
-    if response.status_code == 200:
-        try:
-            results = response.json()
-            subscribers = results.get('data', {}).get('subscribers')
-            if subscribers is not None:
-                return "OK"
-            else:
-                return 0
-        except Exception:
-            return 0
-    else:
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
-
